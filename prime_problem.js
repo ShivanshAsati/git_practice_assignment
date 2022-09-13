b4210e0c7a20252b5e5c26b239b042065feb7e86
Shivansh <asatishivanshshiv@gmail.com>
2022-09-13 20:12:56 +0530

    prime_number

---
+++ prime_problem.js
@@ -0,0 +1,19 @@
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+for (let i=0; i<=arr.length-1; i++){
+  
+  let count==0;
+  
+  for (let j=0; j<=i; j++){
+
+    if(arr[i]%arr[j]==0){
+      count+;
+    }
+    
+  }
+  if (count==1){
+    console.log(arr[i]);
+    
+  }
+
+}
