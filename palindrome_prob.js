d07e80238e825dc53ec97f586ffaab1ab9e697e7
Shivansh <asatishivanshshiv@gmail.com>
2022-09-13 18:33:14 +0530

    added palindrome problem

---
+++ palindrome_prob.js
@@ -0,0 +1,18 @@
+//palindrome problem
+
+let str="NAMAN";
+
+let str1="";
+
+for(let i=0; i<=str.length-1; i++){
+
+str1=str1+str[i];
+}
+
+if(str==str1){
+console.log("Yes");
+}
+else{
+console.log("No");
+}
+
